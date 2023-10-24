b374c9706eef75cb1cda96581ecc3a7e4437f726
Abdessalem-ghodbeni <slouma4ghodbeny@gmail.com>
2023-10-24 15:46:25 +0100

    updateRendezvous using graphQl and java

--- GraphQlLogement(Etudiants)/src/main/java/graphQl/Mutations.java
+++ GraphQlLogement(Etudiants)/src/main/java/graphQl/Mutations.java
@@ -9,13 +9,22 @@ import repository.RendezVousRepository;
 public class Mutations implements GraphQLRootResolver {
     public RendezVousRepository rendezVousRepository;
     public LogementRepository logementRepository;
-    public Mutations(RendezVousRepository rendezVousRepository, LogementRepository logementRepository){
-        this.rendezVousRepository=rendezVousRepository;
-        this.logementRepository=logementRepository;
+
+    public Mutations(RendezVousRepository rendezVousRepository, LogementRepository logementRepository) {
+        this.rendezVousRepository = rendezVousRepository;
+        this.logementRepository = logementRepository;
     }
-    public boolean CreateRendezVous(int id, String date, String heure, int refLog, String numTel){
-        Logement log=logementRepository.getLogementsByReference((refLog));
-                RendezVous rend=new RendezVous(id,date,heure,log,numTel);
-                return rendezVousRepository.addRendezVous(rend);
+
+    public boolean CreateRendezVous(int id, String date, String heure, int refLog, String numTel) {
+        Logement log = logementRepository.getLogementsByReference((refLog));
+        RendezVous rend = new RendezVous(id, date, heure, log, numTel);
+        return rendezVousRepository.addRendezVous(rend);
+    }
+
+    public boolean UpdateRendezVous(int id, String date, String heure, String numTel) {
+        Logement logement = rendezVousRepository.getLogementByRDV(id);
+        RendezVous updateRendezVous = new RendezVous(id, date, heure, logement, numTel);
+        return rendezVousRepository.updateRendezVous(updateRendezVous);
+
     }
 }
